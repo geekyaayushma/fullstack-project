1e83f939f0dbcfb9dd498b4315395bb49374438d
geekyaayushma <mahataayushma653@gmail.com>
2025-10-28 22:05:26 +0545

    Addes homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/common/Navigation";
+import HomePage from "./HomePage";
+import Footer from "./components/common/Footer";
+import BlogList from "./components/blog/BlogList";
+import Blog from "./components/blog/Blog";
+import Team from "./Team";
+import { AuthProvider } from "./components/auth/AuthContext";
+import "./App.css";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PrivateRoute from "./components/auth/PrivateRoute";
+import Courses from "./components/courses/Courses";
+import CourseDetails from "./components/courses/CourseDetails";
+import MyCourses from "./components/courses/MyCourses";
+import CourseContent from "./components/courses/CourseContent";
+import CoursePlayer from "./components/courses/CoursePlayer";
+import Success from "./components/auth/Success";
+import Cancel from "./components/auth/Cancel";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsAuthenticated(!!token);
+  }, []);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <AuthProvider>
+      <Router>
+        <div>
+          <Navigation
+            isAuthenticated={isAuthenticated}
+            onLogout={handleLogout}
+          />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/blogs"
+              element={<PrivateRoute element={<BlogList />} />}
+            />
+            <Route
+              path="/blog/:blogId"
+              element={<PrivateRoute element={<Blog />} />}
+            />
+            <Route path="/team" element={<Team />} />
+            <Route
+              path="/register"
+              element={<Register onLogin={handleLogin} />}
+            />
+            <Route path="/login" element={<Login onLogin={handleLogin} />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/course-details/:id" element={<CourseDetails />} />
+            <Route
+              path="/my-courses"
+              element={<PrivateRoute element={<MyCourses />} />}
+            />
+            <Route
+              path="/start-course/:id"
+              element={<PrivateRoute element={<CourseContent />} />}
+            />
+            <Route
+              path="/course-player/:courseId"
+              element={<PrivateRoute element={<CoursePlayer />} />}
+            />
+            <Route path="/success" element={<Success />} />
+            <Route path="/cancel" element={<Cancel />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
